1fef3d62c6bbba4db500219b4c62fcb7e9e32be0
giopantsulaia <giorgipantsulaia@redberry.ge>
2024-04-18 15:55:09 +0400

    feat: add route controller and form request for numbers

---
+++ app/Http/Controllers/NumberController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PrimeNumbersRequest;
+
+class NumberController extends Controller
+{
+    public function submit(PrimeNumbersRequest $request)
+    {
+
+    }
+}
